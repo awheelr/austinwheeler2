d62be4a5cb5da1f154032df444cf43f3cb37f72f
awheelr <austinskullfnider@gmail.com>
2022-08-09 00:03:03 -0400

    added js to efficently push new photos

--- js/addpictures.js
+++ js/addpictures.js
@@ -1,2 +1,21 @@
 // Whenever a new file is added to the /photos directory
 // get that file path and add it to the html.
+
+{/* <div class="gallery">
+<img src="./images/photo ${number}.jpg" class="lightbox_trigger" alt="no">
+    <div class="desc">buildings</div>
+</div> */}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var number = 8
+
+        for (let i = 0; i < number; i++) {
+            var gallery =
+            '<div class="gallery">' +
+                '<img src="./images/photo' + i + '.jpg" class="lightbox_trigger" alt="no">' + 
+            '</div>';
+
+            var target = document.querySelector(".grid");
+            target.insertAdjacentHTML('beforeend', gallery);
+          }
+});
